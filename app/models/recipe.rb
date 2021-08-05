99404736f80c8eae048673728478c86ebd3ac41e
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-08-05 16:26:52 -0700

    fix errors in pathing

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
-  has_many :ingredient_recipes, inverse_of: :recipe
+  has_many :ingredient_recipes, dependent: :destroy
   has_many :ingredients, :through => :ingredient_recipes
   has_many :recipe_tags, foreign_key: 'tag_id'
   has_many :tags, :through => :recipe_tags
